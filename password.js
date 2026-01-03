dbf78e36a53a001a11aabfa2baeab60bcf944b36
h-uong <157622403+h-uong@users.noreply.github.com>
2026-01-03 11:35:49 -0500

    Add files via upload

---
+++ password.js
@@ -0,0 +1,42 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+const passwordOne = document.getElementById("password-one")
+const passwordTwo = document.getElementById("password-two")
+const Length = 15
+
+let generateBtn = document.getElementById("generate-btn")
+
+const toggleSwitch = document.getElementById("toggleSwitch")
+
+toggleSwitch.addEventListener("change", function(){
+  document.body.classList.toggle("light-mode")
+  document.p.classList.toggle("light-mode")
+})
+
+generateBtn.addEventListener("click", function(){
+  let randomP1 = ""
+  let randomP2 = ""
+
+  for (let i = 0; i < Length; i++) {
+    let randomIndex1 = Math.floor(Math.random()*characters.length)
+    randomP1 += characters[randomIndex1]
+
+    let randomIndex2 = Math.floor(Math.random()*characters.length)
+    randomP2 += characters[randomIndex2]
+  }
+
+  passwordOne.value = randomP1
+  passwordTwo.value = randomP2
+})
+
+passwordOne.addEventListener("click", function() {
+  passwordOne.select()
+  document.execCommand("copy")
+})
+
+passwordTwo.addEventListener("click", function() {
+  passwordTwo.select()
+  document.execCommand("copy")
+})
